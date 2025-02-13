f8bbd7fa62431f082f3e35a42e31cd060b6d9a78
Dasa Srinivas <116433381+srinivas7075@users.noreply.github.com>
2025-02-13 22:46:16 +0530

    Create February-13(2025).java

---
+++ February-13(2025).java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean findTarget(Node root, int target) {
+        List<Integer> inorder = new ArrayList<>();
+        inorderTraversal(root, inorder);
+        int left = 0, right = inorder.size() - 1;
+        while (left < right) {
+            int sum = inorder.get(left) + inorder.get(right);
+            if (sum == target) return true;
+            if (sum < target) left++;
+            else right--;
+        }
+        return false;
+    }
+
+    private void inorderTraversal(Node node, List<Integer> inorder) {
+        if (node == null) return;
+        inorderTraversal(node.left, inorder);
+        inorder.add(node.data);
+        inorderTraversal(node.right, inorder);
+    }
+}
