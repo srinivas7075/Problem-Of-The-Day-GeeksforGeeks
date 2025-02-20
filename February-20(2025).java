9a1331719ab10de3fa200ddd099e8100612ace94
Dasa Srinivas <116433381+srinivas7075@users.noreply.github.com>
2025-02-20 14:05:17 +0530

    Create February-20(2025).java
    Problem name: Find median in a stream

---
+++ February-20(2025).java
@@ -0,0 +1,15 @@
+class Solution {
+    public ArrayList<Double> getMedian(int[] arr) {
+        PriorityQueue<Integer> maxH = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minH = new PriorityQueue<>();
+        ArrayList<Double> res = new ArrayList<>();
+
+        for (int n : arr) {
+            maxH.add(n);
+            minH.add(maxH.poll());
+            if (maxH.size() < minH.size()) maxH.add(minH.poll());
+            res.add(maxH.size() > minH.size() ? (double) maxH.peek() : (maxH.peek() + minH.peek()) / 2.0);
+        }
+        return res;
+    }
+}
