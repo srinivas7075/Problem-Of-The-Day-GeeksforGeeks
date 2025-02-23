0d0e80253047983623c5fc9df56f021ec63edee5
Dasa Srinivas <116433381+srinivas7075@users.noreply.github.com>
2025-02-23 22:03:00 +0530

    Create February-23(2025).java
    Problem Name: Next Greater Element

---
+++ February-23(2025).java
@@ -0,0 +1,14 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int val = arr[i];
+            while (!s.isEmpty() && s.peek() <= val) s.pop();
+            arr[i] = s.isEmpty() ? -1 : s.peek();
+            s.push(val);
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int num : arr) res.add(num);
+        return res;
+    }
+}
