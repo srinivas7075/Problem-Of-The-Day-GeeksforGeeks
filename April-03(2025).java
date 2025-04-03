8db6d1d259a7dc7e13ab987621b9ea2fa64e70e8
Dasa Srinivas <116433381+srinivas7075@users.noreply.github.com>
2025-04-03 23:34:14 +0530

    Create April-03(2025).java
    Problem name: Rotten Oranges

---
+++ April-03(2025).java
@@ -0,0 +1,34 @@
+class Solution {
+    public int orangesRotting(int[][] a) {
+        int n = a.length, m = a[0].length, f = 0, t = 0;
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == 2)
+                    q.add(new int[]{i, j});
+                else if (a[i][j] == 1)
+                    f++;
+            }
+        if (f == 0) return 0;
+        int[][] d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            boolean ch = false;
+            for (int k = 0; k < sz; k++) {
+                int[] p = q.poll();
+                int i = p[0], j = p[1];
+                for (int[] dir : d) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if (x < 0 || y < 0 || x >= n || y >= m || a[x][y] != 1)
+                        continue;
+                    a[x][y] = 2;
+                    q.add(new int[]{x, y});
+                    f--;
+                    ch = true;
+                }
+            }
+            if (ch) t++;
+        }
+        return f == 0 ? t : -1;
+    }
+}
